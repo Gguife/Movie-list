6b28a9898853dd3a6104a46db9a56c6178e9625d
Guilherme Gomes <gguife747@gmail.com>
2023-06-01 22:07:56 -0300

    Dinamic Route

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -2,10 +2,12 @@ import { useRoutes } from "react-router-dom"
 
 //Pages
 import Home  from '../pages/home/Home'
+import MovieDetail from '../pages/movieDetail/MovieDetail'
 
 const MainRouter = () => {
   const router = useRoutes([
-    {path: '/', element: <Home />}
+    {path: '/', element: <Home />},
+    {path: '/movie/:id', element: <MovieDetail />},
   ])
   
   return router
